5db4a56626f82ed5f592db5a83a192cf871303eb
Tatsumi Suenaga <suenaga.1@osu.edu>
2016-04-18 23:33:38 -0400

    Maps and Other stufff

--- app/src/main/java/com/cse/team12/zeroline/MapsActivity.java
+++ app/src/main/java/com/cse/team12/zeroline/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.cse.team12.zeroline;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,13 +16,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    final Context con = this;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -36,12 +42,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        if (isNetworkAvailable()) {
+            mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng chipotle = new LatLng(39.997516, -83.007255);
-        LatLng myLocation;
-        mMap.addMarker(new MarkerOptions().position(chipotle).title("Chipotle"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(chipotle));
+            LatLng chipotle = new LatLng(39.997516, -83.007255);
+            LatLng blaze = new LatLng(39.997123, -83.007178);
+            LatLng diaspora = new LatLng(40.005578, -83.008893);
+            LatLng bib = new LatLng(39.998555, -83.007476);
+            LatLng king = new LatLng(40.002134, -83.015152);
+            LatLng myLocation;
+            mMap.addMarker(new MarkerOptions().position(chipotle).title("Chipotle"));
+            mMap.addMarker(new MarkerOptions().position(blaze).title("Blaze Pizza"));
+            mMap.addMarker(new MarkerOptions().position(diaspora).title("Diaspora"));
+            mMap.addMarker(new MarkerOptions().position(bib).title("Bibibop"));
+            mMap.addMarker(new MarkerOptions().position(king).title("King Asad"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(chipotle));
+
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(king, 15));
+        }
+        else{
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    con);
+
+            // set title
+            alertDialogBuilder.setTitle("Internet Connection not available");
+
+            // set dialog message
+            alertDialogBuilder
+                    .setMessage("Are you seriously trying to use maps without internet connection?" +
+                            " Please uninstall this application.")
+                    .setCancelable(false)
+                    .setPositiveButton("Okay...",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int id) {
+                            // if this button is clicked, close
+                            // current activity
+                            MapsActivity.this.finish();
+                        }
+                    });
+
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
+
+        }
     }
+
+    // Check all connectivities whether available or not
+    public boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        // if no network is available networkInfo will be null
+        // otherwise check if we are connected
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**private boolean isGooglePlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == status) {
+            return true;
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
+            return false;
+        }
+    }**/
 }
